3712b2dc88cef782c2762ea875b1e0f63f468f87
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-15 15:00:08 +0300

    Add attribute name of DataType String and constructor in Individual class

--- src/main/java/Individual.java
+++ src/main/java/Individual.java
@@ -1,1 +1,12 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
+public class Individual {
+private String name;
+
+public Individual(String name) {
+    this.name = name;
+
+}
