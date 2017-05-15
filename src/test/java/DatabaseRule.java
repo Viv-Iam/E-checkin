64d224ac964106039662bdd9386bdb8c7cbf2786
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-15 13:08:10 +0300

    Add data into DatabaseRule.java

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,22 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/e_checkin", "viv-iam", "password");
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteIndividualsQuery = "DELETE FROM individuals *;";
+      String deleteGroupsQuery = "DELETE FROM groups *;";
+      con.createQuery(deleteIndividualsQuery).executeUpdate();
+      con.createQuery(deleteGroupsQuery).executeUpdate();
+    }
+  }
+
+
+}
