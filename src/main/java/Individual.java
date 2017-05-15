dc7d265e255219f72457a274f4e5d6ef3cd2e110
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-15 14:54:48 +0300

    Modify file
    *Import relevant packages in IndividualTest.java
    *Add @Rule annotation into the IndividualTest.java file

--- src/main/java/Individual.java
+++ src/main/java/Individual.java
@@ -1,4 +1,1 @@
-import org.sql2o.*;
-import org.junit.*;
-import static org.junit.Assert.*;
-import java.time.LocalDateTime;
+
---
+++ src/test/java/IndividualTest.java
@@ -0,0 +1,10 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
+public class IndividualTest {
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+}
