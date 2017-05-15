3f5c9a3875cf6f0e2ea1bead0401439d89490ce0
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-15 13:14:41 +0300

    Add test that ensures group object instantiates correctly

--- src/test/java/GroupTest.java
+++ src/test/java/GroupTest.java
@@ -2,3 +2,20 @@ import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
+
+public class GroupTest {
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  private Group mGroup;
+  @Before
+  public void instantiate() {
+    mGroup = new Group();
+  }
+
+  @Test
+  public void group_instantiatesCorrectly_true() {
+  assertEquals(true, mGroup instanceof Group);
+  }
+
+}
