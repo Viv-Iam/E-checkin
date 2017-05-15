516c346e4cd8fa649283cbbd2d8b69da4eaa6da4
cliffgor <clierozigy@gmail.com>
2017-05-15 20:46:35 +0300

    Modifications made: *created an instance of students in studenttest.java

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -15,3 +15,14 @@ public class StudentTest {
     testStudent = new Student("brian", 1);
     secondStudent = new Student("Mow the lawn", 1);
   }
+
+	// creating an instance of Student successfully
+	@Test
+	public void Student_instantiatesCorrectly_true() {
+		assertTrue(testStudent instanceof Student);
+	}
+	// assigning each Student a name and then retrieve it
+	@Test
+	public void Student_instantiatesWithName_String() {
+		assertEquals("brian", testStudent.getName());
+	}
