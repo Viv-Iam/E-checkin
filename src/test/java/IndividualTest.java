c02ee08e028d3fe130dea9419ae6b8e83c9ec61d
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-15 14:57:30 +0300

    Write test that ensures Individual intantiates correctly

--- src/test/java/IndividualTest.java
+++ src/test/java/IndividualTest.java
@@ -7,4 +7,15 @@ public class IndividualTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  private Individual mIndividual;
+  @Before
+  public void instantiate() {
+    mIndividual = new Individual("Vivian");
+  }
+
+  @Test
+  public void Individual_instantiatesCorrectly_true() {
+    assertEquals(true, mIndividual instanceof Individual);
+  }
+
 }
