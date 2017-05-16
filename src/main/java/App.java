2d303b0a18d40a6794eb1411fceda46b596965cc
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-16 09:03:05 +0300

    Import relevant classes into App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
