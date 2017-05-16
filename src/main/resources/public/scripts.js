01d055f2a217b86f833e964acd7992b02aab873b
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-16 15:39:50 +0300

    make way for class register, and increase padding on title of classlist page

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function myFunction() {
   $(".checkedin").hide();
   $(".button").click(function (e) {
     $(".checkedin", this).show();
-    // $("#button1").attr("disabled", true);
     $(".checkin", this).hide();
 });
 });
