fc8eef8393e9c193c49a9b1382457fa07e8ebe20
cliffgor <clierozigy@gmail.com>
2017-05-16 08:00:26 +0300

    Modifications made in Student.java: *created a method that gets; 1)Id 2)Name 3)Category Id form the database

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -21,3 +21,24 @@ public class Student {
           .getKey();
       }
 	}
+	public String getName() {
+	    return name;
+	  }
+
+	  public int getId() {
+	    return id;
+	  }
+
+		public int getCategoryId() {
+        return categoryId;
+      }
+
+		public static Student find(int id) {
+			  try(Connection con = DB.sql2o.open()) {
+			    String sql = "SELECT * FROM students where id=:id";
+			    Student student = con.createQuery(sql)
+			      .addParameter("id", id)
+			      .executeAndFetchFirst(Student.class);
+			    return student;
+			  }
+			}
