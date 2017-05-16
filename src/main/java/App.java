a95abcf89c1c8b997b4baff7b718ee3c1a31ca38
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-16 10:22:59 +0300

    add image

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,5 +27,12 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/classlist/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/classlist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 }
 }
