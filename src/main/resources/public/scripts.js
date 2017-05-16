8382a57e2a04dce98f92ebf8c51c94033b05864f
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-16 15:03:52 +0300

    added toggle functionality for checked in and not checked in property on classlist page

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function myFunction() {
-  $("#button1").click(function (e) {
-    $("span", this).text("Checked In");
-    $("#button1").attr("disabled", true); 
+  $(".checkedin").hide();
+  $(".button").click(function (e) {
+    $(".checkedin", this).show();
+    // $("#button1").attr("disabled", true);
+    $(".checkin", this).hide();
 });
 });
