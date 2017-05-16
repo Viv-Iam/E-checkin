d8e3fa43314b2d8cf42e053e91ffa3eb4383383b
dicksonmuli <dicksonmully6@gmail.com>
2017-05-16 08:11:39 +0300

    overriding equals

--- src/main/java/Sign.java
+++ src/main/java/Sign.java
@@ -4,25 +4,25 @@ import org.sql2o.*;
 import java.sql.Timestamp;
 
 public  class Category {
-	private String name;
 	private int id;
 	private Timestamp timein;
 //contructor
-	public Category(String name, int studentId) {
+	public Category(int studentId) {
 		this.name = name;
 		this.studentId = studentId;
 
+
 		try(Connection con = DB.sql2o.open()) {
-			String sql = "INSERT INTO categories (name) VALUES (:name)";
+			String sql = "INSERT INTO categories (timein, studentId) VALUES (now(), :studentId)";
 			this.id = (int) con.createQuery(sql, true)
-			 .addParameter("name", this.name)
+			 .addParameter("name", this.studentId)
 			 .executeUpdate()
 			 .getKey();
 		}
 	}
 	//getter methods
 
-		public String getName() {
+		public String getStudentId() {
 			 return name;
 		 }
 
@@ -35,3 +35,13 @@ public  class Category {
      //     return con.createQuery(sql).addParameter("id", this.id).executeAndFetch(Student.class);
      //     }
  		// }
+
+		@Override
+	 public boolean equals(Object otherSign){
+		 if (!(otherSign instanceof Sign)) {
+			 return false;
+		 } else {
+			 Sign newSign = (Sign) otherSign;
+			 return this.getPersonId() == newSign.getStudentId();
+		 }
+	 }
