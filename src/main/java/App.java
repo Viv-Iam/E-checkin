8ffab43952ce65b1d1ee0ba08e36256e45a36365
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-17 17:42:10 +0300

    Comment on each method in App.js

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,21 +19,16 @@ public class App {
 
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
-
+//ensures group names enterd are displayed on admin
     get("/admin", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, bject>();
+      Map<String, Object> model = new HashMap<String, Object>();
       model.put("groups", Group.all());
       model.put("template", "templates/admin.vtl");
 
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-//     get("/groups/new", (request, response) -> {
-//   Map<String, Object> model = new HashMap<String, Object>();
-//   model.put("template", "templates/group-form.vtl");
-//   return new ModelAndView(model, layout);
-// }, new VelocityTemplateEngine());
-
+//adds group names to admin page
 post("/groups", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   String name = request.queryParams("name");
@@ -44,6 +39,7 @@ post("/groups", (request, response) -> {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//gets group id
 get("/groups/:id", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   Group group = Group.find(Integer.parseInt(request.params(":id")));
@@ -59,6 +55,7 @@ get("/student-groups", (request, response) -> {
   model.put("template", "templates/student.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
+
 //students in a group
 get("/groups/:id/students", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
@@ -68,12 +65,12 @@ get("/groups/:id/students", (request, response) -> {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//lists all student names on the checkin template
 post("/students/check-in", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   Individual newIndividual = Individual.find(Integer.parseInt(request.queryParams("studentId")));
   Sign sign = new Sign(newIndividual.getId());
   model.put("newIndividual", newIndividual);
-  // model.put("template", "templates/student-list.vtl");
   response.redirect("/student-groups");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
@@ -81,14 +78,12 @@ post("/students/check-in", (request, response) -> {
 //retrieve sign-in with time and student name
 get("/admin/class/register", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
-  // Individual individual = new Individual(:studentId);
   model.put("signs", Sign.all());
-  // model.put("individual", individual);
   model.put("template", "templates/sign-in-list.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
-
+//saves new individual name
 get("groups/:id/individuals/new", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   Group group = Group.find(Integer.parseInt(request.params(":id")));
@@ -97,6 +92,7 @@ get("groups/:id/individuals/new", (request, response) -> {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//saves names of all individuals with ids
 post("/individuals", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
 
@@ -105,13 +101,12 @@ post("/individuals", (request, response) -> {
   String name = request.queryParams("name");
   Individual newIndividual = new Individual(name, group.getId());
   newIndividual.save();
-
   model.put("group", group);
-  // model.put("template", "templates/group-individuals-success.vtl");
   response.redirect("/admin");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//retrieves data of individual after either being updated or deleted
 get("/groups/:group_id/individuals/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Group group = Group.find(Integer.parseInt(request.params(":group_id")));
@@ -122,6 +117,7 @@ get("/groups/:group_id/individuals/:id", (request, response) -> {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+//updates individual name
     post("/groups/:group_id/individuals/:id", (request, response) -> {
 Map<String, Object> model = new HashMap<String, Object>();
 Individual individual = Individual.find(Integer.parseInt(request.params("id")));
@@ -133,6 +129,7 @@ response.redirect(url);
 return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//delete individual
 post("/groups/:group_id/individuals/:id/delete", (request, response) -> {
   HashMap<String, Object> model = new HashMap<String, Object>();
   Individual individual = Individual.find(Integer.parseInt(request.params("id")));
